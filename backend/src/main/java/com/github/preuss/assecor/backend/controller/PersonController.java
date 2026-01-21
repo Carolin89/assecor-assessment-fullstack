146050f6848419b9e37f02daad6a3337c6894c76
Carolin Preuß <carolinpreuss@MacBook-Air-von-Carolin.local>
2026-01-21 14:30:05 +0100

    feat: add person REST controller

---
+++ backend/src/main/java/com/github/preuss/assecor/backend/controller/PersonController.java
@@ -0,0 +1,48 @@
+package com.github.preuss.assecor.backend.controller;
+
+import com.github.preuss.assecor.backend.model.Person;
+import com.github.preuss.assecor.backend.service.PersonService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/persons", produces = "application/json")
+public class PersonController {
+    private final PersonService personService;
+
+    public PersonController(PersonService personService){
+        this.personService = personService;
+    }
+
+    /**
+     * GET /persons
+     */
+    @GetMapping
+    public List<Person> getAllPersons() {
+        return personService.getAllPersons();
+    }
+
+    /**
+     * GET /persons/{id}
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<Person> getPersonById(@PathVariable long id) {
+        return personService.getPersonById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    /**
+     * GET /persons/color/{color}
+     */
+    @GetMapping("/color/{color}")
+    public List<Person> getPersonsByColor(@PathVariable String color) {
+        return personService.getPersonsByColor(color);
+    }
+
+}
