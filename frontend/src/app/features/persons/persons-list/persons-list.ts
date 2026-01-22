9d78c97d99e88c3cf5213f0ecf15fd9c2f37849e
Carolin Preuß <carolin.preussa3@gmail.com>
2026-01-22 14:58:35 +0100

    feat(frontend): display persons list using signals and control flow

--- frontend/src/app/features/persons/persons-list/persons-list.ts
+++ frontend/src/app/features/persons/persons-list/persons-list.ts
@@ -1,18 +1,42 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 import {PersonApi} from '../../../core/api/person-api';
+import {CommonModule} from '@angular/common';
+import {Person} from '../../../shared/models/person';
 
 @Component({
   selector: 'app-persons-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './persons-list.html',
   styleUrl: './persons-list.css',
 })
 export class PersonsList {
   private personApi = inject(PersonApi);
+
+  readonly persons = signal<Person[]>([])
+  readonly loading = signal(true);
+  readonly error = signal<string | null>(null);
+
   ngOnInit() {
-    this.personApi.getPersons().subscribe(data => {
+    this.loadPersons();
+   /* this.personApi.getPersons().subscribe(data => {
       console.log('Persons from API:', data);
-    });
+    });*/
+  }
+
+  private loadPersons(){
+    this.personApi.getPersons().subscribe(
+      {
+        next: (persons) => {
+          this.persons.set(persons);
+          this.loading.set(false);
+        },
+        error: () => {
+          this.error.set('Personen konnten nicht geladen werden');
+          this.loading.set(false);
+        }
+      }
+    )
   }
 
 }
